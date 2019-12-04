448803e4cefa5c02751d2158671e65adc96fb036
joll05 <44553291+joll05@users.noreply.github.com>
2019-12-04 19:08:33 +0100

    Day 4

--- Day 4/solution2.py
+++ Day 4/solution2.py
@@ -1,22 +1,31 @@
+def CheckConsecutive(index, digits):
+    currentIndex = index - 1
+    sequence = [False, True, True, False]
 
-def CheckPossibility(num):
-    digits = [int(d) for d in str(num)]
+    for i in sequence:
+        #print((digits[currentIndex] != digits[index]) != i)
+        if((digits[currentIndex] == digits[index]) != i):
+            return False
+        currentIndex += 1
 
-    lastDigit = 0
-    hasConsecutive = False
-    for i in digits:
-        if(lastDigit > i):
-            return False
-        if(lastDigit == i):
-            hasConsecutive = True
-        
-        lastDigit = i
-    else:
-        if(hasConsecutive == False):
-            return False
-    
     return True
 
+def CheckPossibility(num):
+    digits = [int(d) for d in str(num)] + [-1] * 5
+
+    hasConsecutive = False
+    for i in range(len(digits) - 5):
+        if(digits[i - 1] > digits[i]):
+            return False
+        if(not hasConsecutive):
+            hasConsecutive = CheckConsecutive(i, digits)
+    else:
+        if(not hasConsecutive):
+            return False
+
+    return True
+
+# Inputs
 Min = 248345
 Max = 746315
 
@@ -25,7 +34,7 @@ total = 0
 # There is definitely a better way of doing this, but I'm just going to test every possible combination
 
 for d in range(Min, Max + 1):
-    if(CheckPossibility(d) == True):
+    if(CheckPossibility(d)):
         total+=1
 
 print(total)
